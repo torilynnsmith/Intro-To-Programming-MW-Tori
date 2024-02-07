f9366032ee4f261f9c7287c153e9014e9c1ddc55
Tori Smith <torilynnsmith@gmail.com>
2024-02-07 18:12:21 -0500

    Made Ball Move

--- Pong_CCNYTori/Assets/Scripts/Ball Controller.cs
+++ Pong_CCNYTori/Assets/Scripts/Ball Controller.cs
@@ -4,10 +4,44 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
+    //M/W Class
+    //GLOBAL VARIALBES
+    public Rigidbody2D rbBall;
+
+    public float force = 200;
+
+    private float xDir;
+    private float yDir; 
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("hello world");
+        //Debug.Log("hello world");
+
+        Vector3 direction = new Vector3(0, 0, 0);
+
+        xDir = Random.Range(0, 2);
+        //Debug.Log("xDir = " + xDir);
+        if (xDir == 0)
+        {
+            direction.x = -1; 
+        } else if (xDir == 1)
+        {
+            direction.x = 1; 
+        }
+
+        yDir = Random.Range(0, 2);
+        if (yDir == 0)
+        {
+            direction.y = -1;
+        }
+        else if (yDir == 1)
+        {
+            direction.y = 1;
+        }
+
+        //add force to start movemnt
+        rbBall.AddForce(direction * force);
     }
 
     // Update is called once per frame
