05bb9b71b295e07d321fe380ae48029ce312a14f
Tori Smith <torilynnsmith@gmail.com>
2024-02-08 10:50:46 -0500

    script comments

--- Pong_CCNYTori/Assets/Scripts/Ball Controller.cs
+++ Pong_CCNYTori/Assets/Scripts/Ball Controller.cs
@@ -4,44 +4,54 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    //M/W Class
+    //CCNY M/W In-Class Script
+
     //GLOBAL VARIALBES
-    public Rigidbody2D rbBall;
+    public Rigidbody2D rbBall; //declare Rigidbody2D variable, set in Inspector via drag and dropping
 
-    public float force = 200;
+    public float force = 200; //decare and set force variable
 
-    private float xDir;
-    private float yDir; 
+    private float xDir; //declare x direction float
+    private float yDir; //declare y direction float
 
     // Start is called before the first frame update
     void Start()
     {
-        //Debug.Log("hello world");
+        //This is a comment! 
+        //Debug.Log("hello world"); //print to console
 
-        Vector3 direction = new Vector3(0, 0, 0);
+        Vector3 direction = new Vector3(0, 0, 0); //declare and set a new Vector 3 variable
+                                                  //Vector3 (x,y,z) = represenation of 3D vectors and points (can still be used in 2D projects!)
 
-        xDir = Random.Range(0, 2);
+        //Set direction.x (which way the ball moves horizontally)
+        xDir = Random.Range(0, 2); //pick a random int b/w 0 & 1
         //Debug.Log("xDir = " + xDir);
-        if (xDir == 0)
+        if (xDir == 0) //if 0...
         {
-            direction.x = -1; 
-        } else if (xDir == 1)
+            direction.x = -1; //...set direction.x = -1, then go left
+            //Debug.Log("Ball went Left");
+        }
+        else if (xDir == 1) //if 1...
         {
-            direction.x = 1; 
+            direction.x = 1; //...set direction.x = 1, then go right
+            //Debug.Log("Ball went Right");
         }
 
-        yDir = Random.Range(0, 2);
-        if (yDir == 0)
+        //Set direction.y (which way the ball moves veritcally)
+        yDir = Random.Range(0, 2); //pick a random int b/w 0 & 1
+        if (yDir == 0) //if 0...
         {
-            direction.y = -1;
+            direction.y = -1; //..set direction.y = -1, go down
+            //Debug.Log("Ball went down");
         }
-        else if (yDir == 1)
+        else if (yDir == 1) //if 1...
         {
-            direction.y = 1;
+            direction.y = 1; //..set direction.y = -1, go up
+            //Debug.Log("Ball went up");
         }
 
         //add force to start movemnt
-        rbBall.AddForce(direction * force);
+        rbBall.AddForce(direction * force); //launch the ball (apply force in a direction on the RigidBody)
     }
 
     // Update is called once per frame
