af39740d61bec00b6ea9f29721dfe776f342d103
Tori Smith <torilynnsmith@gmail.com>
2024-02-23 09:00:48 -0500

    code comments

--- Pong_CCNYTori/Assets/Scripts/GameManager.cs
+++ Pong_CCNYTori/Assets/Scripts/GameManager.cs
@@ -9,12 +9,12 @@ public class GameManager : MonoBehaviour
 
     //GLOBAL VARIABLES
     //game objects for UI Elements
-    public TextMeshProUGUI player1ScoreText;
-    public TextMeshProUGUI player2ScoreText;
+    public TextMeshProUGUI player1ScoreText; //Text Object for Player 1 Score, set in inspector
+    public TextMeshProUGUI player2ScoreText; //Text Object for Player 2 Score, set in inspector
 
     //SCORES
-    private int player1Score = 0;
-    private int player2Score = 0; 
+    private int player1Score = 0; //declare and set integer variable for Player 1 Score
+    private int player2Score = 0; //declare and set integer variable for Player 2 Score
 
     // Start is called before the first frame update
     void Start()
@@ -25,17 +25,22 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //update the player scores
         player1ScoreText.text = "P1: " + player1Score;
         player2ScoreText.text = "P2: " + player2Score;
     }
 
+    //When Player 1 Scores, run this function
+    //NOTE: This is a PUBLIC void
     public void Player1Scored()
     {
-        player1Score++; 
+        player1Score++; //add 1 to score value
     }
 
+    //When Player 2 Scores, run this function
+    //NOTE: This is a PUBLIC void
     public void Player2Scored()
     {
-        player2Score++; 
+        player2Score++; //add 1 to score value
     }
 }
--- Pong_CCNYTori/Assets/Scripts/Goal.cs
+++ Pong_CCNYTori/Assets/Scripts/Goal.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class Goal : MonoBehaviour
 {
     //MW CCNY IN CLASS
+
     //GLOBAL VARIABLES
-    public bool isPlayer1Goal;
-    public GameManager myManager;
+    public bool isPlayer1Goal; //declare and set bool in the inspector to determine which Goal is for Player 1
+    public GameManager myManager; //declare and set the Game Manager from the Inspector (will pull the script from the object)
 
     // Start is called before the first frame update
     void Start()
@@ -21,18 +22,21 @@ public class Goal : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    //EVENTS UPON ENTERING/"COLLIDING" WITH A TRIGGER
+    private void OnTriggerEnter2D(Collider2D collision)//MAKE SURE THIS IS 2D!!! Won't work without the 2D qualifier
+                                                       //you could also do this as a true collision on the walls, but I want you to see how triggers work!
+                                                       //If a collider is a trigger, objects will pass right through it, but a "collision" will still be registered
     {
-        if (collision.gameObject.tag == "Ball")
+        if (collision.gameObject.tag == "Ball") //if the Ball collides with the Goal areas, do something
         {
             //Debug.Log("ball in trigger");
-            if (isPlayer1Goal)
+            if (isPlayer1Goal) //if the Trigger is on the Player 1 Goal... (check your bool!)
             {
-                myManager.Player2Scored();
+                myManager.Player2Scored(); //...call the Player2Scored() funciton from the Game Manager
             }
-            else
+            else //if the Trigger is on the Player 2 Goal...
             {
-                myManager.Player1Scored(); 
+                myManager.Player1Scored(); //...call the Player1Scored() funciton from the Game Manager
             }
         }
     }
