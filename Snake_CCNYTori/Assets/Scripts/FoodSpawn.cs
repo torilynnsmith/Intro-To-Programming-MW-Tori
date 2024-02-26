b84fdb73b2914102233f49e7b6117851caaff0de
Tori Smith <torilynnsmith@gmail.com>
2024-02-26 18:33:41 -0500

    snake instantiate

---
+++ Snake_CCNYTori/Assets/Scripts/FoodSpawn.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodSpawn : MonoBehaviour
+{
+    //MW CCNY
+
+    //GLOBAL VARIABLES
+    public GameObject foodPrefab;
+
+    //Border positions
+    public Transform wallTop;
+    public Transform wallBottom;
+    public Transform wallLeft;
+    public Transform wallRight; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Invoke("Spawn", 4);
+        InvokeRepeating("Spawn", 3, 4);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Spawn()
+    {
+        //Debug.Log("Spawn Called");
+
+        int xPos = (int)Random.Range(wallLeft.position.x, wallRight.position.x);
+        int yPos = (int)Random.Range(wallTop.position.y, wallBottom.position.y);
+
+        Instantiate(foodPrefab, new Vector3(xPos, yPos, 0), Quaternion.identity);
+    }
+}
