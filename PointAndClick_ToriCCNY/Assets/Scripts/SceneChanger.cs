18d43902b7c59ce7e58b9ccb9062d3169cb720a7
Tori Smith <torilynnsmith@gmail.com>
2024-03-13 12:28:01 -0400

    comments updated

--- PointAndClick_ToriCCNY/Assets/Scripts/SceneChanger.cs
+++ PointAndClick_ToriCCNY/Assets/Scripts/SceneChanger.cs
@@ -8,7 +8,7 @@ public class SceneChanger : MonoBehaviour
     //CCNY MW IN CLASS
 
     //GLOBAL VARIABLES
-    public int sceneNumber;
+    public int sceneNumber; //essentially does the same thing as sceneID in the Build Settings
         //0 = StartScene
         //1 = MainScene
         //2 = EndScene
@@ -24,32 +24,38 @@ public class SceneChanger : MonoBehaviour
     {
         if (sceneNumber == 0) //we're in the start scene
         {
-            StartSceneControls(); 
+            StartSceneControls(); //call StartSceneControls()
         } else if (sceneNumber == 1) //we're in the main scene
         {
-            MainSceneControls(); 
+            MainSceneControls(); //call MainSceneControls()
         } else if (sceneNumber == 2) //we're in the end scene
         {
-            EndSceneControls(); 
+            EndSceneControls(); //call EndSceneControls()
         }
 
     }
 
     public void StartSceneControls()
     {
-        if (Input.GetKeyDown(KeyCode.Return))
+        if (Input.GetKeyDown(KeyCode.Return)) //if Return/Enter is pressed...(You can change this key code!)
         {
-            SceneManager.LoadScene("MainScene");
+            SceneManager.LoadScene("MainScene"); //load the MainScene
         }
     }
 
     public void MainSceneControls()
     {
-        Debug.Log("main scene controls");
+        if (Input.GetKeyDown(KeyCode.Return)) //if Return/Enter is pressed...(You can change this key code!)
+        {
+            SceneManager.LoadScene("EndScene"); //load the EndScene
+        }
     }
 
     public void EndSceneControls()
     {
-        Debug.Log("end scene controls"); 
+        if (Input.GetKeyDown(KeyCode.Return)) //if Return/Enter is pressed...(You can change this key code!)
+        {
+            SceneManager.LoadScene("MainScene"); //load the StartScene
+        }
     }
 }
