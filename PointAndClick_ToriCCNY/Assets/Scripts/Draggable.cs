639e90fd2d0ffa63559b0a5ad153ccecd8142025
Tori Smith <torilynnsmith@gmail.com>
2024-03-27 12:38:11 -0400

    comments

--- PointAndClick_ToriCCNY/Assets/Scripts/Draggable.cs
+++ PointAndClick_ToriCCNY/Assets/Scripts/Draggable.cs
@@ -6,40 +6,59 @@ public class Draggable : MonoBehaviour
 {
     //IN CLASS CCNY WM
 
+    //NOTE: Your draggable sprites/game objects must have COLLIDERS!
+    //This script lives on the object you are dragging
+
     //GLOBAL VARIABLES
-    private bool isDragging = false;
-    private Vector3 offset; 
+    private bool isDragging = false; //set true/false variable to check if the object is currently being dragged or not.
+    private Vector3 offset; //will store the difference b/w the draggable object's center and the clicked point on the camera's view
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        //if the object is being dragged
         if (isDragging)
         {
+            //Debug.Log("isDragging");
             transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset;
+            //Get the Main Camera object
+            //Screen To World Point: Transforms a point from "screen space" into "world space". Needs a Vector 3.
+            //Use the Input system (event system) to get the current mousePosition. This will be your Vector3 for ScreenToWorldPoint
+
+            //NOTE: w/o the offset variable, the player would have to click in the exact center of the draggable object to get it to move OR IT WOULD JUMP AROUND ON THE Z-axis.
+            //The offset variable account for that error! 
         }
     }
 
     //DRAG
-    private void OnMouseDown()
+    private void OnMouseDown() //OnMouseDown is called when the mouse button is pressed over a UI element or collider
     {
-        //Debug.Log("mouse down");
-        offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        isDragging = true;
+        //Debug.Log("mouse clicked");
+        offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);         //Record the difference b/w the draggable object's center and the clicked point on the camera's view
+        isDragging = true; //set bool to true, the object is currently being dragged
         //Debug.Log("isDragging = " + isDragging);
     }
 
     //STOP DRAGGING
-    private void OnMouseUp()
+    private void OnMouseUp() //OnMouseUp is called when the mouse button is released
     {
-        //Debug.Log("mouse up");
-        isDragging = false;
+        //Debug.Log("mouse released");
+        isDragging = false; //set bool to false, the object is no longer being dragged
         //Debug.Log("isDragging = " + isDragging);
 
     }
+
+    //Other things you might want to do with a draggable sprite!
+    //Keep the draggable sprites w/i the game scene using colliders as borders or hardcoding it in (like pong!)
+    //when the player drags a sprite into a certain area, do something! (change points, change scenes, destroy the sprite and make a new one, etc.)
+    //On Trigger Enter and On Collision will be your friends here!
+
+    //There is another drag scripting object that uses raycasting. We're not going to worry about that during this class, but if you're curious I'll send you a tutorial
+
 }
--- PointAndClick_ToriCCNY/Assets/Scripts/MoveToMouse.cs
+++ PointAndClick_ToriCCNY/Assets/Scripts/MoveToMouse.cs
@@ -7,24 +7,33 @@ public class MoveToMouse : MonoBehaviour
     //IN CLASS CCNY MW
 
     //GLOBAL VARIABLES
-    public float speed = 5f;
-    private Vector3 target; 
+    public float speed = 5f; //set speed, can change in inspector
+    private Vector3 target; //declare target variable, where the moving object will try to move to
 
     // Start is called before the first frame update
     void Start()
     {
-        target = transform.position; 
+        target = transform.position; //set the target to the starting position of the object (it won't be moving at first)
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        //TO DO: A CHECK TO SEE IF AN ITEM IS BEING DRAGGED OR NOT TO KEEP THIS FROM CHASING THE MOUSE POSITION
+
+        if (Input.GetMouseButtonDown(0)) //if left mouse button is clicked...
+                                         //similar to GetKey stuff! GetMouseButtonDown only returns for the first frame the mouse button is clicked
+                                         //0 = Left Mouse, 1 = Right Mouse
         {
             Debug.Log("click to move");
-            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            target.z = transform.position.z; 
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition); //similar to our Draggable Sprite script!
+
+            target.z = transform.position.z; //LOCK THE Z AXIS FOR 2D GAMES
+            //Debug.Log("target =" + target); 
         }
+        //MOVE THE OBJECT
         transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        //MoveTowards calculates a position b/w the starting position and the target position, moving there as quickly as set by the speed. 
     }
 }
