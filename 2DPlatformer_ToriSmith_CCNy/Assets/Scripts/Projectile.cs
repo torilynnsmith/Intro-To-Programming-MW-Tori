33b0f545697ebae5df8ef21b1bbf4864ff8abdd1
Tori Smith <torilynnsmith@gmail.com>
2024-04-17 15:30:39 -0400

    projectile comments

--- 2DPlatformer_ToriSmith_CCNy/Assets/Scripts/Projectile.cs
+++ 2DPlatformer_ToriSmith_CCNy/Assets/Scripts/Projectile.cs
@@ -7,44 +7,52 @@ public class Projectile : MonoBehaviour
     //IN CLASS CCNY MW
 
     //GLOBAL VARIABLES
-    public Rigidbody2D projectileRb;
-    public float speed = 5;
+    public Rigidbody2D projectileRb; //declare and set the Rigidbody for the Projectile in the inspector
+    public float speed = 4; //declare how fast the projectile will go (alter in inspector)
 
     //projectile countdown timer stuff
-    public float projectileLife = 2;
-    public float projectileCount; 
+    public float projectileLife = 2; //how long the projectile will last, 2 seconds
+    public float projectileCount; //counts down time until the projectile destorys itself. (
+                                  //currenlty set to 0
 
     // Start is called before the first frame update
     void Start()
     {
-        projectileCount = projectileLife; 
+        projectileCount = projectileLife; //set projectileCount equal to projectile Life
     }
 
     // Update is called once per frame
     void Update()
     {
-        projectileCount -= Time.deltaTime;
+        projectileCount -= Time.deltaTime; //reduces the projectileCount w/ each second
 
-        if (projectileCount <= 0)
+        if (projectileCount < 0) //if the projectileCount runs out...
         {
-            Destroy(gameObject); 
+            Destroy(gameObject); //destroy the gameObject this script is on (the projectile) 
         }
     }
 
     private void FixedUpdate()
-    {
-        projectileRb.velocity = new Vector3(speed, projectileRb.velocity.y, 0);
+    //FixedUpdate() is called every fixed framerate frame. It runs exactly 50 times per second all the time.
+    //it's better practice to use FixedUpdate when applying force, torque, physics-related stuff b/c it's executed in sync with the physics engine, , not the framerate
+    //IN SHORT: Moving w/ Rigidbody stuff -> FixedUpdate()
+    //VS: Moving w/ transform -> Update()
 
+    {
+        projectileRb.velocity = new Vector3(speed, projectileRb.velocity.y, 0); //shoot to the right according to your Vector3
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        //NOTE: The RigidBody on the projectile must be DYNAMIC for this collision to register. at least one object in any given collision has to be Dynamic
+
+        //if projectile collides w/ an enemy
         if (collision.gameObject.tag == "Lava")
         {
-            //Debug.Log("projectile hit lava");
-            Destroy(collision.gameObject); 
+            //Debug.Log("projectile hit lava rock");
+            Destroy(collision.gameObject);  //destroy the object this projectile collided with
         }
 
-        Destroy(gameObject);
+        Destroy(gameObject); //Destroy the projectile
     }
 }
--- 2DPlatformer_ToriSmith_CCNy/Assets/Scripts/ProjectileLaunch.cs
+++ 2DPlatformer_ToriSmith_CCNy/Assets/Scripts/ProjectileLaunch.cs
@@ -6,30 +6,38 @@ public class ProjectileLaunch : MonoBehaviour
 {
     //IN CLASS CCNY MW
 
-    //GLOBAL VARIABLES
-    public GameObject projectilePrefab;
-    public Transform launchPoint;
+    //THIS SCRIPT LIVES ON THE PLAYER, NOT THE LAUNCH POINT
 
-    //cooldown timer stuff
-    public float shootTime = 0.5f;
-    public float shootCount; 
+    //GLOBAL VARIABLES
+    public GameObject projectilePrefab; //declare and set the projectilePrefab GameObject in the Inspector
+    public Transform launchPoint; //the position from which a projectile will launch
+
+    public float shootTime = 0.5f; //cooldown time amount b/w projectile firing, set in inspector
+                                   //currently set to 0.5
+    public float shootCount; //the cooldown timer itself, set in inspector
 
     // Start is called before the first frame update
     void Start()
     {
-        shootCount = shootTime; 
+        shootCount = shootTime; //set the shootCounter timer equal to shootTime when the scene starts
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) &&  shootCount <= 0)
+        //if the Left Mouse Button is pressed AND the shootCounter is reset to 0 or less than 0
+        //if(Input.GetButtonDown("Fire1") && shootCounter <= 0)
+        if (Input.GetMouseButtonDown(0) && shootCount <= 0)
         {
             Instantiate(projectilePrefab, launchPoint.position, Quaternion.identity);
+            //Instantiate needs 3 things! (1,2,3)
+            //1. what are you instantiating?
+            //2. Where are you instantiating it?
+            //3. What is the rotation you're instantiating it with? USE Quaternion.identity
 
-            shootCount = shootTime; 
+            shootCount = shootTime; //restart the cooldown time to disable ability to shoot
         }
 
-        shootCount -= Time.deltaTime; 
+        shootCount -= Time.deltaTime; //reduce cooldown time
     }
 }
