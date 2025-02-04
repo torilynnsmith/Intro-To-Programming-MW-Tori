3a5484f2838df55523af5072d93331d4b5918e95
Tori Smith <torilynnsmith@gmail.com>
2025-02-04 14:39:57 -0500

    idk update

---
+++ ToriSmith_IntroGame/Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //GLOBAL VARIABLES
+    public bool isMoving = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("Hello Urban Scholars!");
+        isMoving = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("Hello Urban Scholars!"); 
+    }
+}
